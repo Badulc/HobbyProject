b70743c2bba4e32cb7c08766d25fe0effa66f956
badulchowdhury <badulchowdhury@protonmail.com>
2022-04-19 15:51:04 +0100

    seperated crud funtionality into a SnkrService class and package to adhere to OOP principles

--- src/main/java/com/qa/hobbyproject/web/SneakerDomController.java
+++ src/main/java/com/qa/hobbyproject/web/SneakerDomController.java
@@ -16,53 +16,56 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.hobbyproject.domain.SneakerDom;
 
-//import com.example.demo.service.VehicleService;
+import com.qa.hobbyproject.service.SnkrService;
 
 @RestController // annotation to tell spring this is a controller
 //Rest Compliant or REST API
 
 public class SneakerDomController {
 
+	private SnkrService service;
+
+	@Autowired // tell spring to fetch SnkrService from the context
+	// dependency injection
+	public SneakerDomController(SnkrService service) {
+		super();
+		this.service = service;
+	}
+
 	// endpoints
 	// creating https response codes
 	// @GetMapping()
 
-	// list for storing data without db
-	private List<SneakerDom> sneakers = new ArrayList<>();
-
 	// CRUD functionality
-	//repsonse entity is an extension of HttpEntity
-	//that represents http reponses including status code,headers and body
+	// repsonse entity is an extension of HttpEntity
+	// that represents http reponses including status code,headers and body
 
 	@PostMapping("/createSnkr") // 201 - created
 	// create sneaker list
 	public ResponseEntity<SneakerDom> createSneaker(@RequestBody SneakerDom s) {
-		// add sneaker to list
-		this.sneakers.add(s);
-		// get sneaker from list by index in a var called created
-		SneakerDom created = this.sneakers.get(this.sneakers.size() - 1);
-		//create a response
+		SneakerDom created = this.service.createSneaker(s);
+		// create a response
 		ResponseEntity<SneakerDom> response = new ResponseEntity<SneakerDom>(created, HttpStatus.CREATED);
-		// returning created sneaker object
+		// returning response code
 		return response;
 	}
 
 	// read all from list method
 	@GetMapping("/getAllSnkr") // 200 - ok
-	public ResponseEntity<List<SneakerDom>> getAllSneakers(){
-		return ResponseEntity.ok(this.sneakers);
+	public ResponseEntity<List<SneakerDom>> getAllSneakers() {
+		return ResponseEntity.ok(this.service.getAllSnkrs());
 	}
 
 	// read one from list
-	@GetMapping("/getSnkr/{id}") //200 - ok
+	@GetMapping("/getSnkr/{id}") // 200 - ok
 	public SneakerDom getSneaker(@PathVariable Integer id) {
-		return this.sneakers.get(id);
+		return this.service.getSnkr(id);
 	}
 
 	// update method
 	@PutMapping("/replaceSnkr/{id}") // 202 - accepted
-	public ResponseEntity<SneakerDom> replaceSneaker(@PathVariable Integer id, @RequestBody SneakerDom rp) {
-		SneakerDom body = this.sneakers.set(id, rp);
+	public ResponseEntity<SneakerDom> replaceSneaker(@PathVariable Integer id, @RequestBody SneakerDom repSnkr) {
+		SneakerDom body = this.service.replaceSnkr(id, repSnkr);
 		ResponseEntity<SneakerDom> response = new ResponseEntity<SneakerDom>(body, HttpStatus.ACCEPTED);
 		return response;
 	}
@@ -70,9 +73,8 @@ public class SneakerDomController {
 	// delete from list
 	@DeleteMapping("/removeSnkr/{id}") // 204 - no content
 	public ResponseEntity<?> removeSneaker(@PathVariable Integer id) {
-		this.sneakers.remove(id.intValue());
+		this.service.removeSnkr(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
-	
 
 }
